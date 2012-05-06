98daf30bf415d3ec63f305419cb633e2e4480cc6
davidmichaelball@gmail.com <davidmichaelball@gmail.com@06f58045-0f34-d9f0-d44d-afe0706048fb>
2012-05-06 12:00:55 +0000

    major update added vo works on the irat and st lucia datasets

--- src/main_vt.cpp
+++ src/main_vt.cpp
@@ -42,6 +42,8 @@ using namespace std;
 #include <ratslam_ros/ViewTemplate.h>
 #include <ros/console.h>
 
+#include <image_transport/image_transport.h>
+
 #include "ratslam/Visual_Template_Match.h"
 
 #if HAVE_IRRLICHT
@@ -50,100 +52,49 @@ ratslam::ViewTemplateScene *vts = NULL;
 bool use_graphics;
 #endif
 
-ratslam::Visual_Template_Match * vt = NULL;
-
-
 boost::property_tree::ptree settings;
 
 using namespace ratslam;
 
-void camera_callback(sensor_msgs::ImageConstPtr image, ratslam::Visual_Template_Match *vt, ros::Publisher * pub_vt)
+void image_callback(sensor_msgs::ImageConstPtr image, ros::Publisher * pub_vt)
 {
   static ratslam_ros::ViewTemplate vt_output;
+  static ratslam::Visual_Template_Match * vt = NULL;
 
   ROS_DEBUG_STREAM("VT:camera_callback{" << ros::Time::now() << "} seq=" << image->header.seq);
 
-  if (!::vt)
+  if (!vt)
   {
     boost::property_tree::ptree ratslam_settings;
     get_setting_child(ratslam_settings, settings, "ratslam", true);
     ratslam_settings.put("image_width", image->width);
     ratslam_settings.put("image_height", image->height);
-    ::vt = new ratslam::Visual_Template_Match(ratslam_settings);
-        
+    vt = new ratslam::Visual_Template_Match(ratslam_settings);
+
 #ifdef HAVE_IRRLICHT
-  boost::property_tree::ptree draw_settings;
-  get_setting_child(draw_settings, settings, "draw", true);
-  get_setting_from_ptree(use_graphics, draw_settings, "enable", true);
+    boost::property_tree::ptree draw_settings;
+    get_setting_child(draw_settings, settings, "draw", true);
+    get_setting_from_ptree(use_graphics, draw_settings, "enable", true);
     if (!::vts && use_graphics)
-      ::vts = new ratslam::ViewTemplateScene(draw_settings, ::vt);
+    ::vts = new ratslam::ViewTemplateScene(draw_settings, vt);
 #endif
   }
 
   if (image->encoding == "bgr8")
-    ::vt->on_image(&image->data[0], false);
+    vt->on_image(&image->data[0], false);
   else
-    ::vt->on_image(&image->data[0], true);
+    vt->on_image(&image->data[0], true);
 
   vt_output.header.stamp = ros::Time::now();
   vt_output.header.seq++;
-  vt_output.current_id = ::vt->get_current_vt();
-
-  pub_vt->publish(vt_output);
-
-#ifdef HAVE_IRRLICHT
-	if (use_graphics)
-	{
-		vts->draw_all();
-	}
-#endif
-}
-
-void compcamera_callback(sensor_msgs::CompressedImageConstPtr image, ratslam::Visual_Template_Match *vt, ros::Publisher * pub_vt)
-{
-  static ratslam_ros::ViewTemplate vt_output;
-
-  ROS_DEBUG_STREAM("VT:camera_callback{" << ros::Time::now() << "} seq=" << image->header.seq);
-
-  cv::Mat image_mat_jpeg(image->data.size(), 1, CV_8UC1, (void*) &image->data[0]);
-  cv::Mat imdecoded = cv::imdecode(image_mat_jpeg, 1);
-
-  if (!::vt)
-  {
-    boost::property_tree::ptree ratslam_settings;
-    get_setting_child(ratslam_settings, settings, "ratslam", true);
-    ratslam_settings.put("image_width", imdecoded.cols);
-    ratslam_settings.put("image_height", imdecoded.rows);
-    ::vt = new ratslam::Visual_Template_Match(ratslam_settings);
-
-#ifdef HAVE_IRRLICHT
-  boost::property_tree::ptree draw_settings;
-  get_setting_child(draw_settings, settings, "draw", true);
-  get_setting_from_ptree(use_graphics, draw_settings, "enable", true);
-    if (!::vts && use_graphics)
-      ::vts = new ratslam::ViewTemplateScene(draw_settings, ::vt);
-#endif
-  }
-
-  ::vt->on_image(imdecoded.data, false);
-
-#if 0
-  if (image->encoding == "bgr8")
-    ::vt->on_image(&image->data[0], false);
-  else
-    ::vt->on_image(&image->data[0], true);
-#endif
-
-  vt_output.header.stamp = ros::Time::now();
-  vt_output.header.seq++;
-  vt_output.current_id = ::vt->get_current_vt();
+  vt_output.current_id = vt->get_current_vt();
 
   pub_vt->publish(vt_output);
 
 #ifdef HAVE_IRRLICHT
   if (use_graphics)
   {
-	  vts->draw_all();
+    vts->draw_all();
   }
 #endif
 }
@@ -153,15 +104,16 @@ int main(int argc, char * argv[])
 
   if (argc < 2)
   {
-    std::cout << "USAGE: " << argv[0] << " <config_file>" << std::endl;
+    ROS_FATAL_STREAM("USAGE: " << argv[0] << " <config_file>");
     exit(-1);
   }
+  std::string topic_root = "";
 
-//  boost::property_tree::ptree settings, ratslam_settings, draw_settings;
+  boost::property_tree::ptree general_settings;
   read_ini(argv[1], settings);
 
- // get_setting_child(ratslam_settings, settings, "ratslam", true);
- // get_setting_child(draw_settings, settings, "draw", true);
+  get_setting_child(general_settings, settings, "general", true);
+  get_setting_from_ptree(topic_root, general_settings, "topic_root", (std::string)"");
 
   if (!ros::isInitialized())
   {
@@ -169,21 +121,11 @@ int main(int argc, char * argv[])
   }
   ros::NodeHandle node;
 
-  std::string topic_root = "";
-
   ros::Publisher pub_vt = node.advertise<ratslam_ros::ViewTemplate>(topic_root + "/ViewTemplate/Template", 0);
 
-  ros::Subscriber sub_camera = node.subscribe<sensor_msgs::Image>(topic_root + "/camera/image_rect", 1,
-                                                                  boost::bind(camera_callback, _1, vt, &pub_vt),
-                                                                  ros::VoidConstPtr(),
-                                                                  ros::TransportHints().tcpNoDelay());
+  image_transport::ImageTransport it(node);
+  image_transport::Subscriber sub = it.subscribe(topic_root + "/camera/image", 1, boost::bind(image_callback, _1, &pub_vt));
 
-  ros::Subscriber sub_compcamera = node.subscribe<sensor_msgs::CompressedImage>(topic_root + "/camera/compressedimage_rect", 1,
-                                                                  boost::bind(compcamera_callback, _1, vt, &pub_vt),
-                                                                  ros::VoidConstPtr(),
-                                                                  ros::TransportHints().tcpNoDelay());
-
-  // TODO: the draw all should go in here, only the updates in teh callback
   ros::spin();
 
   return 0;
